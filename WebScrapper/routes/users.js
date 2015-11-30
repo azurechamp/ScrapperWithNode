ccc4532fa419ac1a965b3ec56fff21121fc3f049
Saad Mahmood <saadmehmood21@gmail.com>
2015-11-30 16:10:01 +0500

    Main Commit

---
+++ WebScrapper/routes/users.js
@@ -0,0 +1,39 @@
+﻿var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var router = express.Router();
+
+var url = "http://www.thedailystar.net";
+
+router.get('/', function (req, res) {
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode === 200) {
+			var data = scrapeDataFromHtml(body);
+			res.send(data);
+		}
+		return console.log(error);
+	});
+});
+
+var scrapeDataFromHtml = function (html) {
+	var data = {};
+	
+	var $ = cheerio.load(html);
+	var sectionTitle = $('a', '.creem-box').first().text().trim();
+	var heading = $('h2 > a', '.creem-box').text().trim();
+	var imageSource = $('a > img', '.creem-box').attr("src");
+	var description = $('.intro', '.creem-box').text().trim();
+	var fullNewsLink = $('a', '.creem-box').eq(2).attr("href");
+
+	data = {
+		sectionTitle: sectionTitle,
+		heading : heading,
+		imageSource: imageSource,
+		description: description,
+		fullNewsLink : url + fullNewsLink
+	};
+	return data;
+};
+
+
+module.exports = router;
